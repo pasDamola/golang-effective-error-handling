e5c0c024e41a1852fd56c80a81029b404b8dd3a9
oyinda <oyincode@Featuremind-Laptop.local>
2025-04-12 00:37:24 +0100

    contextual errors

--- main.go
+++ main.go
@@ -15,9 +15,17 @@ type Truck struct {
 	id string
 }
 
+func (t *Truck) LoadCargo() error {
+	return ErrTruckNotFound
+}
+
 // processTruck handles the loading and unloading of a truck
 func processTruck(truck Truck) error {
 	fmt.Printf("Processing Truck %s\n", truck.id)
+
+	if err := truck.LoadCargo(); err != nil {
+		return fmt.Errorf("Error truck not foind %w", err)
+	}
 	return ErrNotImplemented
 }
 
